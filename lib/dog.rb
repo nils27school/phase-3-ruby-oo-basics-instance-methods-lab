e31d0371a1d49f879d55b2334620420203e89275
nilssvensson <nils27school@gmail.com>
2022-11-01 17:01:14 -0400

    commit tree f0c98c989995f5e0d79c9cb0f0f7016457ce0d2c

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+# class Dog
+    # Class body
+  
+    # Instance Method Definition
+    class Dog
+        def bark
+          puts "Woof!"
+      end
+      
+    #   fido = Dog.new
+    #   fido.bark #=> "Woof!"
+
+    def sit
+        puts "The Dog is sitting"
+    end
+  end
